7f0c69c54c797973932cb502016017e82eea9b21
Jonathan <jonathanelian64@gmail.com>
2023-07-20 22:11:26 -0700

    feat: rv1960 lost

--- src/routers/rv60.ts
+++ src/routers/rv60.ts
@@ -16,6 +16,11 @@ router_rv60.get("/chapter/:book/:chapter", (c: Context) => {
 	return getChapterVersion(c, 'verses_rv1960');
 })
 
+router_rv60.get("/book/:book/:chapter", (c: Context) => {
+	return getChapterVersion(c, 'verses_rv1960');
+})
+
+
 router_rv60.get("/oldTestament/:book/:chapter", (c: Context) => {
 
 	return getOldTestamentBook(c, Path.RV60);
