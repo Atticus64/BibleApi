b2defb9b519cbcb945b592b0f82598bdc90f37d5
Jonathan <jonathanelian64@gmail.com>
2023-07-23 21:02:01 -0700

    change to not ssl

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,11 @@
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 import postgres from "https://deno.land/x/postgresjs@v3.3.5/mod.js";
 
-
 export function connect() {
 	const dev = Deno.env.get("DEVELOP") ?? ""
-	if (dev !== "") {
-		return postgres(Deno.env.get("DATABASE_URL") ?? "");
-	}
-	const sql = postgres(Deno.env.get("DATABASE_URL") ?? "", { ssl: { rejectUnauthorized: false } });
+	// if (dev !== "") {
+		// return postgres(Deno.env.get("DATABASE_URL") ?? "");
+	// }
+	const sql = postgres(Deno.env.get("DATABASE_URL") ?? "", { ssl: false });
 	return sql;
 }
