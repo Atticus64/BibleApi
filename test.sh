1cd3cc1350222cf2d87fd0be50f7aa423427f1c6
Jonathan <jonathanelian64@gmail.com>
2023-09-18 10:08:25 -0700

    fix: timmings ci

--- test.sh
+++ test.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+#set -eu
 
 # init server
- docker build -t deno_app . && docker run -p 8000:8000 --name api_bible --detach deno_app
+docker build -t deno_app . && docker run -p 8000:8000 --name api_bible --detach deno_app
+
 
 echo "Waiting for server to start"
 until curl -s -f -o /dev/null "http://localhost:8000"
 do
-  sleep 5
+  echo "Testing $1..."
+  sleep 10
 done
+
+echo "Server running at http://localhost:8000"
 
 # tests
 echo "Running tests"
-hurl --test tests/*.hurl
+hurl --variable host="$1" --test tests/*.hurl
 
 # Stop server
 echo "Stopping server"
