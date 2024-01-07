2c3e9a375d8229f3ca1b95a3f14c6a8fc0548902
Jonathan <jonathanelian64@gmail.com>
2024-01-07 15:21:23 -0700

    feat: new validation book

--- src/utils/book.ts
+++ src/utils/book.ts
@@ -1,37 +1,73 @@
 import { books } from "$/constants.ts";
 
 const oldTestamentbooks = books.filter((b) => {
-  return b.testament === "Antiguo Testamento";
+	return b.testament === "Antiguo Testamento";
 });
 
 const newTestamentbooks = books.filter((b) => {
-  return b.testament === "Nuevo Testamento";
+	return b.testament === "Nuevo Testamento";
 });
 
 export const isAbbreviation = (book: string) => {
 	return books.some((b) => {
-		return b.abrev === book.toUpperCase()
-	})
-}
+		return b.abrev === book.toUpperCase();
+	});
+};
 
 export function existBook(book: string): boolean {
-	return books.some(b => b.name.toLowerCase() === book.toLowerCase())
+	const exists = books.some((bk) => {
+		return bk.names.some((name) => {
+			return name.toLowerCase() === book.toLowerCase();
+		});
+	});
+
+	return exists;
+}
+
+function getName(names: string[], book: string) {
+	return names.find((n) => {
+		if (n.toLowerCase() === book.toLowerCase()) {
+			return n;
+		}
+	});
 }
 
 export const getNameByAbbreviation = (book: string) => {
-	return books.find((b) => {
-		return b.abrev === book.toUpperCase()
-	})?.name
-}
+	const bk = books.find((b) => {
+		if (b.abrev === book.toUpperCase()) {
+			return b;
+		}
+	});
+
+	if (!bk) {
+		return;
+	}
+
+	return bk.names[0];
+};
 
 export const isInOldTestament = (book: string) => {
-  return oldTestamentbooks.some((b) => b.name.toLowerCase() === book.toLowerCase());
+	return oldTestamentbooks.some((b) => {
+		b.names.forEach((n) => {
+			return n.toLowerCase() === book.toLowerCase();
+		});
+	});
 };
 
 export const isInNewTestament = (book: string) => {
-  return newTestamentbooks.some((b) => b.name.toLowerCase() === book.toLowerCase());
+	return newTestamentbooks.some((b) => {
+		b.names.forEach((n) => {
+			return n.toLowerCase() === book.toLowerCase();
+		});
+	});
 };
 
 export const getInfoBook = (book: string) => {
-  return books.find((b) => b.name.toLowerCase() === book.toLowerCase()) || books[0];
+	const bk = books.find((b) => {
+		if (b.names.includes(book)) {
+			return b;
+		}
+	});
+
+	return bk ?? books[0];
 };
